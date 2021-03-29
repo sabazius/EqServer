c0e4cac9f9d6e72bf1a7429f48e9b08677fca3ca
Vasil Ivanov <vasivanov.info@gmail.com>
2021-03-29 17:26:35 +0300

    add auto mapper extension

---
+++ EqServer/Extensions/AutoMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EqServer.Extensions
+{
+    public class AutoMapping : Profile
+    {
+        public AutoMapping()
+        {
+
+        }
+    }
+}
